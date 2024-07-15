57a43c3215e0277a917e6d0c4ccc77ce3f872dd2
ElisaB59 <lisa.beauprez.59@gmail.com>
2024-07-15 18:57:36 +0200

    add world-gen_merger script

---
+++ world-gen_merger/README.md
@@ -0,0 +1,3 @@
+Script to merge deepslate and generic ore spawn statistics for JEI graphics on modpacks.
+
+Written and tested with Python 3.12.4
---
+++ world-gen_merger/merger.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import collections, functools, operator
+from collections import Counter
+import decimal
+
+# create a new context for this task
+ctx = decimal.Context()
+#own converter to avoid scientific notation (e-x)
+def float_to_str(f):
+    d1 = ctx.create_decimal(repr(f))
+    return format(d1, 'f')
+
+filename = input("Enter file path: ")
+f = open(filename)
+data = json.load(f)
+f.close
+finalData = []
+
+for item in data:
+    block = item["block"].replace(":", ":deepslate_")
+    distrib = Counter({i.split(",")[0]: float(i.split(",")[1]) for i in item["distrib"][:-1].split(";")})
+    dim = item["dim"]
+    deepOre = [ore1 for ore1 in data if (ore1["dim"] == dim
+									  and ore1["block"] == block)]
+    if len(deepOre) > 0 :
+        deepOre = deepOre[0]
+        deepDistrib = Counter({i.split(",")[0]: float(i.split(",")[1]) for i in deepOre["distrib"][:-1].split(";")})
+        dictArray = [distrib, deepDistrib]
+        realDistrib = distrib + deepDistrib
+        distrib.update(realDistrib)
+        finalDistrib = ';'.join([key+","+float_to_str(value) for key, value in distrib.items()])
+        item["distrib"] =  finalDistrib
+        deepOre["distrib"] = finalDistrib	
+    if "ore" in item["block"]:
+        finalData.append(item)
+            	 
+
+with open(filename+".merged", 'w') as f:
+    json.dump(finalData, f, indent=4), 
